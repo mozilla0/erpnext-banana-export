33381e0d878d2b895ded6ea4cf887b00d69ff7a6
Matthias <matthias@hummler.ch>
2026-03-31 17:21:52 +0200

    Fix: setup.cfg ergänzt und Frappe-Standardverzeichnisse hinzugefügt
    esbuild-Fehler beim Build auf Frappe Cloud behoben.
    
    Changed:
    - setup.cfg hinzugefügt (Frappe-Standard für Metadaten/Dependencies)
    - setup.py auf Minimalformat reduziert
    - banana_export/templates/includes/ hinzugefügt
    - banana_export/www/ hinzugefügt
    - banana_export/public/css/ hinzugefügt

--- setup.py
+++ setup.py
@@ -1,16 +1,9 @@
 from setuptools import setup, find_packages
 
-with open("requirements.txt") as f:
-    install_requires = f.read().strip().split("\n")
-
 setup(
     name="banana_export",
     version="1.0.0",
-    description="Banana Buchhaltung CSV Export für Joker IT AG",
-    author="Joker IT AG",
-    author_email="info@jokerit.ch",
     packages=find_packages(),
     zip_safe=False,
     include_package_data=True,
-    install_requires=install_requires,
 )
